d7e497c11174d0b4a122d8f9a7ed09b0773856d0
Rander Carlos <randerccf@gmail.com>
2017-08-18 17:13:39 -0300

    Desenvolvido a listagem de hóspedes

--- src/AppBundle/Controller/HospedeController.php
+++ src/AppBundle/Controller/HospedeController.php
@@ -5,15 +5,27 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 class HospedeController extends Controller
 {
 
+    /**
+     * @Route("/hospedes", name="hospede.index")
+     * @Method("GET")
+     */
     public function indexAction(Request $request)
     {
+        $hospedes = [];
+        $hospedes[] = ['titulo' => 'Sr.', 'nome' => 'Agostinho Carrara', 'email' => 'acarraga@gmail.com'];
+        $hospedes[] = ['titulo' => 'Sra.', 'nome' => 'Júlia Santos', 'email' => 'jsantos@gmail.com'];
+        $hospedes[] = ['titulo' => 'Sr.', 'nome' => 'Pedro Cardoso', 'email' => 'pcardoso@gmail.com'];
+        $hospedes[] = ['titulo' => 'Sra.', 'nome' => 'Maria Silvia', 'email' => 'msilvia@gmail.com'];
+        $hospedes[] = ['titulo' => 'Sr.', 'nome' => 'Alan Pedro', 'email' => 'apedro@gmail.com'];
+
         // replace this example code with whatever you need
         return $this->render('hospede/index.html.twig', [
-
+            'hospedes' => $hospedes
         ]);
     }
 }
